5f206bae5dfc9e753d9d2dce4de28872a53570fc
Thiago Lins <thiagolinsx@gmail.com>
2015-02-18 20:18:11 -0200

    Consertando abas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,9 @@
 //= require foundation
 //= require_tree .
 
-// $(function(){ $(document).foundation(); });
-$(function(){
-    $(window).load(function() {
-        $(document).foundation();
-    });
-});
+ $(function(){ $(document).foundation(); });
+//$(function(){
+//    $(window).load(function() {
+//        $(document).foundation();
+//    });
+//});
